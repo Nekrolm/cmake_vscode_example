258bd3b9388165baea0582b0acf078efdbf19ca4
Dmis <sv.l1@mail.ru>
2019-10-10 22:18:13 +0300

    libraries examples

---
+++ src/algo_libs/include/algo.h
@@ -0,0 +1,7 @@
+#pragma once
+
+int add(int x, int y);
+
+int sub(int x, int y);
+
+int mul(int x, int y);
---
+++ src/tests_for_libs/algo_test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+#include "algo.h"
+
+
+
+int main(){
+
+    printf("2 + 2 = %d\n", add(2, 2));
+
+    printf("2 - 2 = %d\n", sub(2, 2));
+
+    printf("2 * 2 = %d\n", mul(2, 2));
+
+    return 0;
+}
